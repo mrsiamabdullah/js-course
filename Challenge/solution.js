b66bd6b8e4faccfa5eefc5839f2322cab8266407
Siam Abdullah <mrsiamabdullah@gmail.com>
2025-04-14 11:29:14 +0600

    Initial commit

---
+++ Challenge/solution.js
@@ -0,0 +1,9 @@
+//? Personal budget tracker:
+let userName = "Md Ruhul Amin";
+console.log("User: ", userName); // User Name
+
+let userIncome = 120000;
+console.log("User Income: ", userIncome); // User Income
+
+let userExpenses = 100000;
+console.log("User Expense: ", userExpenses); // User Expense
---
+++ Lesson-1/script.js
@@ -0,0 +1,110 @@
+// alert("Good Night!");
+// alert("Siam Abdullah!");
+// console.log(10 + 5);
+// console.log(20 - 5);
+// console.log(2 + 2 - 5);
+
+// document.body.innerHTML = "Good Night! Siam Abdullah!";
+// document.body.innerHTML = "Siam Abdullah is learning JavaScript!";
+// document.body.innerHTML = "";
+// console.log(10 + 8 + 20);
+// console.log(100 - 50 - 20 + 200);
+
+// console.log("Welcome to the world of Javascript!");
+// console.log("Good Night!");
+
+/// Data types ///
+
+// let cartQuantity = 0;
+
+// const name = "Siam";
+// console.log(`My name is ${name}`);
+
+// const totalBill = 5 + 3 * 2 + 9;
+// console.log(`Cost of food: $${totalBill}`);
+
+// tax = totalBill * 0.1;
+// console.log(`Tax (10%): $${tax} `);
+
+// const totalWithTax = totalBill + tax;
+// console.log(`Total cost: $${totalWithTax}`);
+
+// let calculation = "";
+
+// document.write("Name: Siam Abdullah </br>");
+// document.write("Phone: 0199999999 </br>");
+// document.write("Country: Bangladesh");
+
+// let fullName = "Siam Abdullah is coding for the future";
+// console.log(fullName);
+
+// let valueNumber = 123333.324;
+// console.log(valueNumber);
+
+//? Personal budget tracker:
+// let userName = "Md Ruhul Amin";
+// console.log(userName); // User Name
+
+// let userIncome = 120000;
+// console.log(userIncome); // User Income
+
+// let userExpenses = 100000;
+// console.log(userExpenses); // User Expense
+
+/// Number Methods ///
+
+// let number = 34.433;
+// number = toString(number);
+// number = parseInt(number);
+// number = parseFloat(number);
+// number = number.toFixed(2);
+// number = number.toPrecision(3);
+
+// console.log(typeof Number("12.23"));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(number);
+
+// let x = 9.656;
+// x.toFixed(0);
+// x.toFixed(2);
+// x.toFixed(4);
+// x.toFixed(6);
+// console.log(x);
+
+/// String concatenation ///
+
+// let firstName = "Siam";
+// let lastName = "Abdullah";
+
+// console.log("My name is", firstName, lastName);
+// console.log(
+//   firstName + " is my first name " + "and " + lastName + " is my last name"
+// );
+
+/// Library functions for string ///
+
+// let userInput = prompt("Enter your text here: ");
+// console.log(userInput.length);
+
+// let sentence = "I love my parents and younger brothers";
+// let concat1 = "This is a ";
+// let concat2 = "full sentence";
+
+// concatenation = concat1.concat(concat2);
+// convertUppercase = sentence.toUpperCase();
+// convertTolowercase = sentence.toLowerCase();
+
+// console.log(convertUppercase);
+// console.log(convertTolowercase);
+// console.log(concatenation);
+// console.log(convertTolowercase.charAt(5));
+
+// let getUserFirstName = prompt("Enter your first name: ");
+// let getUserLastName = prompt("Enter your last name: ");
+
+// addNames = getUserFirstName.concat(" ", getUserLastName);
+// console.log(addNames);
+// console.log("Total length of full name:", addNames.length);
+// console.log("Converting full name into Uppercase:", addNames.toUpperCase());
+// console.log("2nd position of the name is", addNames.charAt(2));
