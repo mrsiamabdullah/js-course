ab98cd7100c8a2e8b584966227fea7b2ae4fc49a
Siam Abdullah <mrsiamabdullah@gmail.com>
2025-04-16 19:26:09 +0600

    Initial commit

--- Challenge/solution.js
+++ Challenge/solution.js
@@ -2,8 +2,10 @@
 let userName = "Md Ruhul Amin";
 console.log("User: ", userName); // User Name
 
-let userIncome = 120000;
+let userIncome = 8000;
 console.log("User Income: ", userIncome); // User Income
 
-let userExpenses = 100000;
+let userExpenses = 5000;
 console.log("User Expense: ", userExpenses); // User Expense
+
+let;
