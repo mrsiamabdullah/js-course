6e6753bcc26bc45a8aeba8ab71210c965108dbb3
Siam Abdullah <mrsiamabdullah@gmail.com>
2025-04-16 19:24:17 +0600

    Initial commit

--- Lesson-2/skibidi.js
+++ Lesson-2/skibidi.js
@@ -1,1 +1,33 @@
+// Arithmetic operator
 
+//> +, -, *, /, ++, --, **, %
+
+// Assignment operator
+
+//> =, +=, -=, *=, /=, %=, **=
+
+// let x = 5;
+// x += 3; // x = x + 3
+
+// console.log(x);
+
+// Making a basic calculator
+let numberOne, numberTwo;
+
+numberOne = prompt("Enter your first number: ");
+numberTwo = prompt("Enter your second number: ");
+
+numberOne = Number(numberOne);
+numberTwo = Number(numberTwo);
+
+addition = numberOne + numberTwo;
+subtraction = numberOne - numberTwo;
+multiplication = numberOne * numberTwo;
+division = numberOne / numberTwo;
+remainder = numberOne % numberTwo;
+
+document.write(numberOne, " + ", numberTwo, " = ", addition, "</br>");
+document.write(numberOne, " - ", numberTwo, " = ", subtraction, "</br>");
+document.write(numberOne, " * ", numberTwo, " = ", multiplication, "</br>");
+document.write(numberOne, " / ", numberTwo, " = ", division, "</br>");
+document.write(numberOne, " % ", numberTwo, " = ", remainder, "</br>");
