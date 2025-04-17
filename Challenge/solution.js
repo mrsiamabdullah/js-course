12e7ebf19ca6fff95ddd7edc632b947096facd09
Siam Abdullah <mrsiamabdullah@gmail.com>
2025-04-17 19:30:09 +0600

    Initial commit

--- Challenge/solution.js
+++ Challenge/solution.js
@@ -1,11 +1,42 @@
 //? Personal budget tracker:
+
+// User name
 let userName = "Md Ruhul Amin";
-console.log("User: ", userName); // User Name
+console.log(`User: ${userName}`); // User Name
 
+// User income
 let userIncome = 8000;
-console.log("User Income: ", userIncome); // User Income
+console.log(`User Income: $${userIncome}`); // User Income
 
-let userExpenses = 5000;
-console.log("User Expense: ", userExpenses); // User Expense
+// Tex deduction
+let taxCalculation = userIncome * 0.1; // tax 10%
+console.log(`Tax Deducted 10%: $${taxCalculation}`);
 
-let;
+// Net income after tax
+let netIncome = userIncome - taxCalculation;
+console.log(`Net Income after Tax: $${netIncome}`);
+
+// Calculate the expense
+let carService, groceries, entertainment, tuitionFees, homeRent, salami;
+
+carService = 650;
+groceries = 300;
+entertainment = 350;
+tuitionFees = 1200;
+homeRent = 2000;
+salami = 500;
+
+total =
+  carService + groceries + entertainment + tuitionFees + homeRent + salami;
+
+// Total expense
+let userExpenses = total;
+console.log(`User Expense: $${userExpenses}`); // User Expense
+
+// Remaining balance
+let remainingBalance = netIncome - userExpenses;
+console.log(`Remaining Balance: $${remainingBalance}`);
+
+// Savings
+let savings = remainingBalance * 0.2;
+console.log(`Saving (20% of Balance): $${savings}`);
--- Lesson-2/skibidi.js
+++ Lesson-2/skibidi.js
@@ -12,22 +12,56 @@
 // console.log(x);
 
 // Making a basic calculator
-let numberOne, numberTwo;
+// let numberOne, numberTwo;
 
-numberOne = prompt("Enter your first number: ");
-numberTwo = prompt("Enter your second number: ");
+// numberOne = prompt("Enter your first number: ");
+// numberTwo = prompt("Enter your second number: ");
 
-numberOne = Number(numberOne);
-numberTwo = Number(numberTwo);
+// numberOne = Number(numberOne);
+// numberTwo = Number(numberTwo);
 
-addition = numberOne + numberTwo;
-subtraction = numberOne - numberTwo;
-multiplication = numberOne * numberTwo;
-division = numberOne / numberTwo;
-remainder = numberOne % numberTwo;
+// addition = numberOne + numberTwo;
+// subtraction = numberOne - numberTwo;
+// multiplication = numberOne * numberTwo;
+// division = numberOne / numberTwo;
+// remainder = numberOne % numberTwo;
 
-document.write(numberOne, " + ", numberTwo, " = ", addition, "</br>");
-document.write(numberOne, " - ", numberTwo, " = ", subtraction, "</br>");
-document.write(numberOne, " * ", numberTwo, " = ", multiplication, "</br>");
-document.write(numberOne, " / ", numberTwo, " = ", division, "</br>");
-document.write(numberOne, " % ", numberTwo, " = ", remainder, "</br>");
+// document.write(numberOne, " + ", numberTwo, " = ", addition, "</br>");
+// document.write(numberOne, " - ", numberTwo, " = ", subtraction, "</br>");
+// document.write(numberOne, " * ", numberTwo, " = ", multiplication, "</br>");
+// document.write(numberOne, " / ", numberTwo, " = ", division, "</br>");
+// document.write(numberOne, " % ", numberTwo, " = ", remainder, "</br>");
+
+//? Relational Operator
+// let data = 33 > 43;
+// console.log(data);
+
+// let value = 43 < 50;
+// console.log(value);
+
+// console.log(6 >= 6);
+// console.log(11 <= 11);
+
+// let String = "pen";
+// let String2 = "paper";
+
+// console.log(String > String2);
+// console.log(String >= String2);
+// console.log(String < String2);
+
+// console.log(3 == "3");
+// console.log(3 === "3");
+// console.log(3 === 3);
+
+// Logical operator
+let x = 44;
+let y = 23;
+let z = 33;
+
+console.log(z > y && x > y && y < z);
+console.log(z > y && x > y && y > z);
+console.log(z < y || x < y || y < z);
+console.log(z < y || x < y || y > z);
+
+console.log(!(x < y));
+console.log(!(x > y));
