6c6ffc7a4a0bea9ebd4256f138e9315ce7a964e4
Siam Abdullah <mrsiamabdullah@gmail.com>
2025-04-26 12:05:28 +0600

    Initial commit

--- Lesson-3/js.js
+++ Lesson-3/js.js
@@ -46,3 +46,76 @@
 // } else {
 //   console.log("The letter is consonant =", letter);
 // }
+
+// Ternary Operator
+// let defineNumber = prompt("Enter a number: ");
+
+// defineNumber = defineNumber > 0 ? "Positive" : "Negative";
+// console.log(defineNumber);
+
+// let number = prompt("Enter your result number: ");
+
+// if (number > 100) console.log("Please enter a valid number!");
+// else if (number >= 80) console.log("A+");
+// else if (number >= 70) console.log("A");
+// else if (number >= 60) console.log("A-");
+// else if (number >= 50) console.log("B");
+// else if (number >= 40) console.log("C");
+// else if (number >= 33) console.log("D");
+// else console.log("Fail");
+
+// number =
+//   number > 100
+//     ? "Please enter a valid number!"
+//     : number >= 80
+//     ? "A+"
+//     : number >= 70
+//     ? "A"
+//     : number >= 60
+//     ? "A-"
+//     : number >= 50
+//     ? "B"
+//     : number >= 40
+//     ? "C"
+//     : number >= 33
+//     ? "D"
+//     : "Fail";
+// console.log(number);
+
+// switch, case, break, default
+const numberSpelling = prompt("Enter a single number between 1-9: ");
+
+switch (numberSpelling) {
+  case "0":
+    console.log("Zero");
+    break;
+  case "1":
+    console.log("One");
+    break;
+  case "2":
+    console.log("Two");
+    break;
+  case "3":
+    console.log("Three");
+    break;
+  case "4":
+    console.log("Four");
+    break;
+  case "5":
+    console.log("Five");
+    break;
+  case "6":
+    console.log("Six");
+    break;
+  case "7":
+    console.log("Seven");
+    break;
+  case "8":
+    console.log("Eight");
+    break;
+  case "9":
+    console.log("Nine");
+    break;
+  default:
+    console.log("Not a valid number!");
+}
